54bcfb90a7257e49ee9bc3e7ec032b9a7cdf467e
Alexey Lapin <alexey-lapin@protonmail.com>
2021-02-09 18:15:13 +0300

    add gradle

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    java
+    id("com.github.ben-manes.versions")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.1")
+}
+
+tasks {
+    dependencyUpdates {
+        checkConstraints = true
+        resolutionStrategy {
+            componentSelection {
+                all {
+                    val rejected = listOf("alpha", "beta", "rc", "cr", "m", "preview", "b", "ea")
+                        .map { qualifier -> Regex("(?i).*[.-]$qualifier[.\\d-+]*") }
+                        .any { it.matches(candidate.version) }
+                    if (rejected) {
+                        reject("Release candidate")
+                    }
+                }
+            }
+        }
+    }
+}
+
---
+++ settings.gradle.kts
@@ -0,0 +1,8 @@
+pluginManagement {
+    plugins {
+        id("com.github.ben-manes.versions") version "0.36.0"
+    }
+}
+
+rootProject.name = "java-prep-11"
+
