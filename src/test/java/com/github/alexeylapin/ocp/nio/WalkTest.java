14e81e4ca8661a6a766591140592057ad0573dca
Alexey Lapin <alexey-lapin@protonmail.com>
2021-10-21 20:40:42 +0300

    add paths tests

--- src/test/java/com/github/alexeylapin/ocp/nio/WalkTest.java
+++ src/test/java/com/github/alexeylapin/ocp/nio/WalkTest.java
@@ -1,2 +1,19 @@
-package com.github.alexeylapin.ocp.nio;public class WalkTest {
+package com.github.alexeylapin.ocp.nio;
+
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class WalkTest {
+
+    @Test
+    void name() throws Exception{
+        Files.walk(Path.of(""));
+        Files.find(Path.of(""), 1, null);
+
+        Files.walkFileTree(Path.of(""), null);
+        Files.newDirectoryStream(Path.of(""));
+    }
+
 }
