f8e79da24feda22e9d892667a1b3fc3c5ea58f14
Alexey Lapin <alexey-lapin@protonmail.com>
2021-11-29 22:56:37 +0100

    add opional test

---
+++ src/test/java/com/github/alexeylapin/ocp/functional/OptionalTest.java
@@ -0,0 +1,17 @@
+package com.github.alexeylapin.ocp.functional;
+
+import org.junit.jupiter.api.Test;
+
+public class OptionalTest {
+
+    @Test
+    void methods() {
+        // optional.get()
+        // optional.ifPresent()
+        // optional.isPresent()
+        // optional.orElse()
+        // optional.orElseGet()
+        // optional.orElseThrow()
+    }
+
+}
