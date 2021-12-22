4c7dd1db7b8d41733d26ccb49602257a428af188
Alexey Lapin <alexey-lapin@protonmail.com>
2021-12-22 19:40:48 +0100

    refactor date time formatter tests

--- src/test/java/com/github/alexeylapin/ocp/datetime/FormatterTest.java
+++ src/test/java/com/github/alexeylapin/ocp/datetime/FormatterTest.java
@@ -1,7 +1,33 @@
 package com.github.alexeylapin.ocp.datetime;
 
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class FormatterTest {
 
     // m, M, d, D, e, y, s, S, h, H, and z
+
+    @Test
+    void isoDateFormatter() {
+        DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE;
+
+        String formatted = formatter.format(LocalDate.of(2022, 5, 5));
+
+        assertThat(formatted).isEqualTo("2022-05-05");
+    }
+
+    @Test
+    void isoDateTimeFormatter() {
+        DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE_TIME;
+
+        String formatted = formatter.format(LocalDateTime.of(2022, 5, 5, 16, 20, 0));
+
+        assertThat(formatted).isEqualTo("2022-05-05T16:20:00");
+    }
 
 }
