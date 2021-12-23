2b7fcb9456036694fbf355d3111fc4f88b13f186
Alexey Lapin <alexey-lapin@protonmail.com>
2021-12-23 18:57:15 +0100

    refactor date time formatter tests

--- src/test/java/com/github/alexeylapin/ocp/datetime/FormatterTest.java
+++ src/test/java/com/github/alexeylapin/ocp/datetime/FormatterTest.java
@@ -10,7 +10,6 @@ import java.time.temporal.UnsupportedTemporalTypeException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class FormatterTest {
 
