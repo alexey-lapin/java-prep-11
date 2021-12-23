892ec112644681551be8a956c00cb5f8093c7ab8
Alexey Lapin <alexey-lapin@protonmail.com>
2021-12-23 18:42:19 +0100

    refactor date time formatter tests

--- src/test/java/com/github/alexeylapin/ocp/datetime/FormatterTest.java
+++ src/test/java/com/github/alexeylapin/ocp/datetime/FormatterTest.java
@@ -4,9 +4,13 @@ import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.UnsupportedTemporalTypeException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class FormatterTest {
 
@@ -16,18 +20,26 @@ public class FormatterTest {
     void isoDateFormatter() {
         DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE;
 
-        String formatted = formatter.format(LocalDate.of(2022, 5, 5));
+        LocalDate localDate = LocalDate.of(2022, 5, 5);
+        assertThat(formatter.format(localDate)).isEqualTo("2022-05-05");
 
-        assertThat(formatted).isEqualTo("2022-05-05");
+        LocalTime localTime = LocalTime.of(16, 20, 0);
+        // TemporalAccessor must contain year, month, and date info
+        assertThatExceptionOfType(UnsupportedTemporalTypeException.class)
+                .isThrownBy(() -> formatter.format(localTime));
     }
 
     @Test
     void isoDateTimeFormatter() {
         DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE_TIME;
 
-        String formatted = formatter.format(LocalDateTime.of(2022, 5, 5, 16, 20, 0));
+        LocalDateTime localDateTime = LocalDateTime.of(2022, 5, 5, 16, 20, 0);
+        assertThat(formatter.format(localDateTime)).isEqualTo("2022-05-05T16:20:00");
 
-        assertThat(formatted).isEqualTo("2022-05-05T16:20:00");
+        LocalDate localDate = LocalDate.of(2022, 5, 5);
+        // TemporalAccessor must contain hour, minute, and second info
+        assertThatExceptionOfType(UnsupportedTemporalTypeException.class)
+                .isThrownBy(() -> formatter.format(localDate));
     }
 
 }
